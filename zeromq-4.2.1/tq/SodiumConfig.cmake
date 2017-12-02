70fa4b5fb703c4f38c6d25bd685c1df0d808cc33
Xu Tiezhu <xutiezhu@gmail.com>
2017-12-03 01:27:29 +0800

    fix Windows compile issue;add zlib

--- zeromq-4.2.1/tq/SodiumConfig.cmake
+++ zeromq-4.2.1/tq/SodiumConfig.cmake
@@ -1,6 +1,6 @@
 set(sodium_LIB_DIR         "${CMAKE_BINARY_DIR}/../../libsodium-1.0.12/build/dist/lib")
 
-set(SODIUM_LIBRARIES sodium)
+set(SODIUM_LIBRARIES sodium-static)
 
 include_directories   (
   ${sodium_INCLUDE_DIR}
@@ -11,8 +11,13 @@ include_directories   (
 link_directories      (${sodium_LIB_DIR} )
 
 add_definitions(
-    -DZMQ_USE_LIBSODIUM=1
-    -fPIC
-)
+  -DZMQ_USE_LIBSODIUM=1
+  -DSODIUM_STATIC
+  -DSODIUM_EXPORT=
+  )
+
+if (UNIX)
+  add_definitions(-fPIC)
+endif()
 
 set(SODIUM_FOUND True)
